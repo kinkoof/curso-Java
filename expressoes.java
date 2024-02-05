d55bb676b1bda052720e90d6622cc829640ec3a4
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-05 12:55:49 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   GUI_intro.java
    	new file:   expressoes.java

---
+++ expressoes.java
@@ -0,0 +1,15 @@
+public class expressoes {
+    public static void main(String[] args) {
+        // expressoes = operadores & operacoes
+        // operaçoes =valores, variaveis, numeros, quantidade
+        // operadores = + - * / %
+
+        int amigo = 10;
+
+        amigo = amigo / 3; //apresenta um numero inteiro
+
+
+        System.out.println(amigo);
+
+    }
+}
