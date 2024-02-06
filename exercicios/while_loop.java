f8eceef4d3a0a4f5c592dc9d51458d61b6bda961
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-06 12:41:46 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   exercicios/while_loop.java

--- exercicios/while_loop.java
+++ exercicios/while_loop.java
@@ -13,8 +13,17 @@ public class while_loop {
 
             System.out.print("Digite seu nome: ");
             nome = scanner.nextLine();
-
         }
+
+
+        do{ // o Do sempre vai realizar pelo menos uma vez o conteudo entre {}
+            System.out.print("Digite seu nome: ");
+            nome = scanner.nextLine();
+
+        }while (nome.isBlank() || nome.length() <= 2);
+
+
+
 
         System.out.println("seja bem vindo " + nome);
 
