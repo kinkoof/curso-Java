5e1061b9b8ef833a94e3c795b33d370ee485a269
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-06 14:31:52 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   exercicios/arrays.java

---
+++ exercicios/arrays.java
@@ -0,0 +1,26 @@
+package exercicios;
+
+public class arrays {
+    public static void main(String[] args) {
+        // arrays sao usados para armazenas varios valores em uma unica variavel
+
+        String[] carros = { "camaro", "corvete", "tesla" };
+
+        carros[0] = "mustang";
+
+        System.out.println(carros[0]);
+
+        // ------------------------------------------------------------------------
+
+        String[] motos = new String[4];
+
+        motos[0] = "Yamaha";
+        motos[1] = "Harley";
+        motos[2] = "Ducati";
+
+        for (int i = 0; i < motos.length; i++) {
+            System.out.println(motos[i]);
+
+        }
+    }
+}
