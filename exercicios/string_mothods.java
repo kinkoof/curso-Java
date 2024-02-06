111aca081dfbba0485b507d1902f37a40022199d
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-06 14:59:19 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   exercicios/string_mothods.java

---
+++ exercicios/string_mothods.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+public class string_mothods {
+    public static void main(String[] args) {
+        String nome = "  Lucas de Souza Ferrari ";
+
+        // boolean resultado = nome.equals("Lucas"); //case sensitive
+        // boolean resultado = nome.equalsIgnoreCase("Lucas");
+        // int resultado =nome.length(); //quantidade de caracteres da string
+        // char resultado = nome.charAt(0); //volta a letra na posicao desejada
+        // int resultado = nome.indexOf("s"); //retorna a posicao da letra desejada
+        // boolean resultado = nome.isEmpty();
+        // String resultado = nome.toUpperCase(); // deixa tudo em maiusculo
+        // String resultado = nome.toLowerCase(); // deixa tudo em minusculo
+        // String resultado = nome.trim(); // retira os espaços atras e na frente do texto
+        String resultado = nome.replace("a", "e");
+
+
+        System.out.println(resultado);
+    }
+}
