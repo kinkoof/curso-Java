49c8acd1d729203dcb79f32e8a70b3d62b8ae021
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-06 14:20:33 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   exercicios/for_loop.java
    	new file:   exercicios/nest_loop.java

---
+++ exercicios/for_loop.java
@@ -0,0 +1,11 @@
+package exercicios;
+
+public class for_loop {
+    public static void main(String[] args) {
+
+        for (int i = 100; i >= 0; i-=2) {
+            System.out.println(i);
+
+        }
+    }
+}
---
+++ exercicios/nest_loop.java
@@ -0,0 +1,34 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class nest_loop {
+    public static void main(String[] args) {
+        // ninho de loop = um loop dentro de um loop
+        Scanner scanner = new Scanner(System.in);
+
+        int linha;
+        int coluna;
+        String simbolo = "";
+
+        System.out.println("digite o numero de linha: ");
+        linha = scanner.nextInt();
+
+        System.out.println("digite o numero de colunas: ");
+        coluna = scanner.nextInt();
+
+        System.out.println("Qual simbolo deseja usar?");
+        simbolo = scanner.next();
+
+        for (int i = 1; i <= linha; i++) {
+            System.out.println();
+            for (int j = 1; j <= coluna; j++) {
+                System.out.print(simbolo);
+
+            }
+        }
+
+        scanner.close();
+
+    }
+}
