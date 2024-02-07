549b9fd3dcff21475b871784c108eecaf1636e5f
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-07 15:33:46 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   exercicios/overloaded_methods.java
    	new file:   exercicios/printf.java

--- exercicios/overloaded_methods.java
+++ exercicios/overloaded_methods.java
@@ -1,5 +1,54 @@
 package exercicios;
 
 public class overloaded_methods {
-    
+    public static void main(String[] args) {
+        // overloaded methods = mesmo nome mas com parametros diferentes
+
+        double x = soma(1.4, 3.0);
+        // int y = soma(1, 3, 5);
+        // int z = soma(1, 3, 5, 7);
+
+        System.out.println(x);
+        // System.out.println(y);
+        // System.out.println(z);
+
+    }
+
+    static int soma(int a, int b) {
+        System.out.println("overloaded 1");
+        return a + b;
+    }
+
+    static int soma(int a, int b, int c) {
+        System.out.println("overloaded 2");
+
+        return a + b + c;
+    }
+
+    static int soma(int a, int b, int c, int d) {
+        System.out.println("overloaded 3");
+
+        return a + b + c + d;
+    }
+
+    static double soma(double a, int b) {
+        System.out.println("overloaded 4");
+        return a + b;
+    }
+    static double soma(double a, double b) {
+        System.out.println("overloaded 7");
+        return a + b;
+    }
+
+    static double soma(double a, int b, int c) {
+        System.out.println("overloaded 5");
+
+        return a + b + c;
+    }
+
+    static double soma(double a, int b, int c, int d) {
+        System.out.println("overloaded 6");
+
+        return a + b + c + d;
+    }
 }
---
+++ exercicios/printf.java
@@ -0,0 +1,47 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class printf {
+    public static void main(String[] args) {
+        // printf()= um metodo adicional de controlar, formatar, e mostrar o texto no
+        // console
+        // dois argumentos = formato string + (objeto, variavel, valor)
+        // % [flags] [precision] [width] [conversion-character]
+
+        Scanner scanner = new Scanner(System.in);
+
+        String mystring = "lucas";
+        double mydouble = 1000;
+        char mychar = '@';
+        boolean myboolean = true;
+
+        // [conversion-character]
+        // System.out.printf("exemplo: %d \n", 123); // %d = numeros decimais
+        // System.out.printf("ola %s \n", mystring); // %s = string
+        // System.out.printf("Ponto Flutuante: %f\n", mydouble); // %.2f = double
+        // System.out.printf("Caractere: %c\n", mychar); // %c = char
+        // System.out.printf("Booleano: %b\n", myboolean); // %b = boolean
+
+        System.out.println("\n");
+        // [width] numero minimo que sera mostrado
+        System.out.printf("hello %3s", mystring);
+
+        System.out.println("\n");
+        // [precision] numero de casas decimais quando for double
+        System.out.printf("voce tem %.2f reais", mydouble);
+
+        System.out.println("\n");
+        // [flags] adicona um efeito
+        /*
+         * - : left-justify
+         * + : adiciona um (+) ou (-) no numero
+         * 0 : zero padded
+         * , : separador de numeros maiores que 1000
+         */
+        System.out.printf("voce tem %,+.2f reais", mydouble);
+
+        scanner.close();
+
+    }
+}
