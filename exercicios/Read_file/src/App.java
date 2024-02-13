a4baebf8d63abf85ad505a8dd7a693cd43fcdc36
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-13 13:58:00 -0300

    new file:   exercicios/Read_file/art.txt 	new file:   exercicios/Read_file/bin/App.class 	modified:   exercicios/Read_file/src/App.java

--- exercicios/Read_file/src/App.java
+++ exercicios/Read_file/src/App.java
@@ -1,5 +1,29 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        try {
+            FileReader reader = new FileReader("art.txt");
+            int data = reader.read();
+
+            while (data != -1) {
+                System.out.print((char)data);
+                data = reader.read();
+            }
+
+            reader.close();
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
     }
 }
