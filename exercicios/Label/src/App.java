28c72a1eff0aaaf736721d573e0db751d079a27d
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-13 15:29:05 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   exercicios/Label/.vscode/settings.json
    	new file:   exercicios/Label/README.md
    	new file:   exercicios/Label/bin/App.class
    	new file:   exercicios/Label/bob.png
    	new file:   exercicios/Label/src/App.java

---
+++ exercicios/Label/src/App.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        ImageIcon image = new ImageIcon("bob.png");
+        Border border = BorderFactory.createLineBorder(Color.red, 3);
+
+        JLabel label = new JLabel();
+        label.setText("Como voce esta");
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(Color.red);
+        label.setFont(new Font("MV boli", Font.PLAIN, 50));
+        label.setIconTextGap(-100);
+        label.setBackground(Color.black);
+        label.setOpaque(true);
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        // label.setBounds(100, 100, 250, 250);
+
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setSize(500,500);
+        // frame.setLayout(null);
+
+
+
+        frame.add(label);
+        frame.pack(); //adjusta a tela aos intens dentro
+        frame.setVisible(true);
+    }
+}
