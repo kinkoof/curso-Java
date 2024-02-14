38e95fc7b546384292017ddee15396aecca3166e
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-14 14:50:01 -0300

    Changes to be committed: 	new file:   exercicios/Paineis/.vscode/settings.json 	new file:   exercicios/Paineis/README.md 	new file:   exercicios/Paineis/bin/App.class 	new file:   exercicios/Paineis/definicoes.png 	new file:   exercicios/Paineis/src/App.java 	new file:   "exercicios/bot\303\243o/.vscode/settings.json" 	new file:   "exercicios/bot\303\243o/README.md" 	new file:   "exercicios/bot\303\243o/bin/AncestorListener.class" 	new file:   "exercicios/bot\303\243o/bin/App.class" 	new file:   "exercicios/bot\303\243o/bin/MyFrame.class" 	new file:   "exercicios/bot\303\243o/definicoes.png" 	new file:   "exercicios/bot\303\243o/src/AncestorListener.java" 	new file:   "exercicios/bot\303\243o/src/App.java" 	new file:   "exercicios/bot\303\243o/src/MyFrame.java"

---
+++ exercicios/Paineis/src/App.java
@@ -0,0 +1,61 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Jpanel  = um GUI componente que funciona como um container para outros componentes
+
+        //ImageIcon icon = new ImageIcon("definicoes.png");
+
+
+        JLabel labelGreen = new JLabel();
+        labelGreen.setText("Hello word! mas verde");
+        // label.setIcon(icon);
+        labelGreen.setVerticalAlignment(JLabel.BOTTOM);
+        labelGreen.setHorizontalAlignment(JLabel.RIGHT);
+        labelGreen.setBounds(100,100, 100, 75);
+
+        JLabel labelVermelho = new JLabel();
+        labelVermelho.setText("Hello word! mas vermelho");
+        labelVermelho.setVerticalAlignment(JLabel.BOTTOM);
+        labelVermelho.setHorizontalAlignment(JLabel.RIGHT);
+
+
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0, 0, 250, 250);
+        redPanel.setLayout(null);
+
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250, 0, 250, 250);
+        bluePanel.setLayout(null);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0, 250, 500, 250);
+        greenPanel.setLayout(null);
+
+
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(750,750);
+        greenPanel.add(labelGreen);
+
+
+        frame.setVisible(true);
+        frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(greenPanel);
+
+    }
+}
---
+++ exercicios/botão/src/MyFrame.java
@@ -0,0 +1,54 @@
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+
+public class MyFrame extends JFrame{
+
+    JButton button;
+    JLabel label;
+
+    MyFrame(){
+       // ImageIcon icon = new ImageIcon("definicoes.png");
+
+        label = new JLabel("OMAGAD");
+        label.setBounds(150, 250, 150, 150);
+        label.setVisible(false);
+
+        button = new JButton();
+        button.setBounds(100, 100, 300, 200);
+        //button.addActionListener(e -> System.out.println("Hello, Word!"));
+        //button.addActionListener(e -> button.setEnabled(false)); //permite clicar apenas 1 x
+        button.addActionListener(e -> label.setVisible(true));
+        button.setText("Botão");
+        button.setFocusable(false);
+        button.setHorizontalTextPosition(JButton.LEFT);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setFont((new Font("Comic Sans", Font.BOLD, 25)));
+        button.setForeground(Color.CYAN);
+        button.setBackground(Color.LIGHT_GRAY);
+        button.setBorder(BorderFactory.createEtchedBorder());
+        //button.setEnabled(false);
+
+
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500, 500);
+        this.setVisible(true);
+        this.add(button);
+        this.add(label);
+
+    }
+
+
+
+}
