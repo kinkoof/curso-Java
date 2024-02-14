a971abe37f7780e5a02fc262ef48aaecddc0ac3d
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-14 16:11:42 -0300

    new file:   exercicios/BorderLayout/.vscode/settings.json 	new file:   exercicios/BorderLayout/README.md 	new file:   exercicios/BorderLayout/bin/App.class 	new file:   exercicios/BorderLayout/bin/MyFrame.class 	new file:   exercicios/BorderLayout/src/App.java 	new file:   exercicios/BorderLayout/src/MyFrame.java 	new file:   exercicios/flow layout/.vscode/settings.json 	new file:   exercicios/flow layout/README.md 	new file:   exercicios/flow layout/bin/App.class 	new file:   exercicios/flow layout/src/App.java

---
+++ exercicios/flow layout/src/App.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10,10));
+
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(250, 250));
+        panel.setBackground(Color.lightGray);
+        panel.setLayout(new FlowLayout());
+
+        //JButton botao1 = new JButton();
+        //frame.add(botao1);
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+
+        frame.add(panel);
+
+
+
+
+        frame.setVisible(true);
+    }
+}
