652824c89cf7cd0ac59441ee77f99cdcedceea85
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-20 10:58:13 -0300

    Changes to be committed: 	modified:   exercicios/Selecionar arquivos/bin/Myframe.class 	modified:   exercicios/Selecionar arquivos/src/Myframe.java

--- exercicios/Selecionar arquivos/src/Myframe.java
+++ exercicios/Selecionar arquivos/src/Myframe.java
@@ -1,17 +1,30 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Myframe extends JFrame implements ActionListener{
 
+    JButton button = new JButton("selecione");
+
     Myframe(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button.addActionListener(this);
+
+        this.add(button);
+        this.pack();
+        this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+        if (e.getSource() == button) {
+            
+        }
 
     }
 
