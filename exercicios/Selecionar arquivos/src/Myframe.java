cfe6510f72e58f84bf42e4bbd159983fbf4c3c66
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-20 10:54:50 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   exercicios/Selecionar arquivos/.vscode/settings.json
    	new file:   exercicios/Selecionar arquivos/README.md
    	new file:   exercicios/Selecionar arquivos/bin/App.class
    	new file:   exercicios/Selecionar arquivos/bin/Myframe.class
    	new file:   exercicios/Selecionar arquivos/src/App.java
    	new file:   exercicios/Selecionar arquivos/src/Myframe.java

---
+++ exercicios/Selecionar arquivos/src/Myframe.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Myframe extends JFrame implements ActionListener{
+
+    Myframe(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
