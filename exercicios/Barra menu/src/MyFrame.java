b04c7a0923a120ae7824cad9cb12648b580db19f
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-20 10:51:23 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   exercicios/Barra menu/bin/MyFrame.class
    	modified:   exercicios/Barra menu/src/MyFrame.java

--- exercicios/Barra menu/src/MyFrame.java
+++ exercicios/Barra menu/src/MyFrame.java
@@ -6,6 +6,7 @@ import javax.swing.JMenuItem;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class MyFrame extends JFrame implements ActionListener {
 
@@ -30,6 +31,13 @@ public class MyFrame extends JFrame implements ActionListener {
         saveItem.addActionListener(this);
         exitItem.addActionListener(this);
 
+        loadItem.setMnemonic(KeyEvent.VK_L); // l para carregar
+        saveItem.setMnemonic(KeyEvent.VK_S); // S para salvar
+        exitItem.setMnemonic(KeyEvent.VK_E); // e para sair
+
+        file.setMnemonic(KeyEvent.VK_F); // alt + f para o file
+
+
         file.add(loadItem);
         file.add(saveItem);
         file.add(exitItem);
@@ -49,7 +57,14 @@ public class MyFrame extends JFrame implements ActionListener {
             System.out.println("arquivo carregado");
 
         }
-        else
+        else if (e.getSource() == saveItem) {
+            System.out.println("arquivo salvo");
+        }
+        else if (e.getSource() == exitItem) {
+            System.out.println("saindo...");
+            //this.dispose();
+            System.exit(0);
+        }
     }
 
 }
