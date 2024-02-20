386c21cb48b0d4909d9c32aef1930f388f2e923d
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-20 10:18:27 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   exercicios/Barra de progresso/bin/BarraProgresso.class
    	modified:   exercicios/Barra de progresso/src/BarraProgresso.java

--- exercicios/Barra de progresso/src/BarraProgresso.java
+++ exercicios/Barra de progresso/src/BarraProgresso.java
@@ -1,3 +1,43 @@
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
 public class BarraProgresso {
-    
+
+    JFrame frame = new JFrame();
+    JProgressBar bar = new JProgressBar();
+
+
+    BarraProgresso(){
+
+        bar.setValue(0);
+        bar.setBounds(10,10,380,50);
+        bar.setStringPainted(true);
+        bar.setFont(new Font("MV Boli", Font.BOLD, 25));
+
+
+        frame.add(bar);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+        carregar();
+    }
+
+    public void carregar(){
+        int x = 0;
+
+        while (x<= 100) {
+            bar.setValue(x);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            x++;
+        }
+        bar.setString("Carregado!");
+    }
+
 }
