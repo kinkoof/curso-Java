fabc3ed9fd88bc557ec51c5237c1002c76edb451
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-20 11:17:13 -0300

    Changes to be committed: 	modified:   exercicios/Selecionar arquivos/bin/Myframe.class 	modified:   exercicios/Selecionar arquivos/src/Myframe.java

--- exercicios/Selecionar arquivos/src/Myframe.java
+++ exercicios/Selecionar arquivos/src/Myframe.java
@@ -1,15 +1,16 @@
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
-public class Myframe extends JFrame implements ActionListener{
+public class Myframe extends JFrame implements ActionListener {
 
     JButton button = new JButton("selecione");
 
-    Myframe(){
+    Myframe() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
@@ -23,7 +24,20 @@ public class Myframe extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button) {
-            
+
+            JFileChooser fileChooser = new JFileChooser();
+
+            fileChooser.setCurrentDirectory(new File("."));
+
+
+            // int resp = fileChooser.showOpenDialog(null); // seleciona o arquivo para abrir
+            int resp = fileChooser.showSaveDialog(null); // seleciona o arquivo para abrir
+
+
+            if (resp == JFileChooser.APPROVE_OPTION) {
+                File file = new File( fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+            }
         }
 
     }
